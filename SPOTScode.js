92d913aced254692b914b92f027764259e3baf5a
RI45816 <uu3@umbc.edu>
2017-04-06 04:25:28 -0400

    @76IVC@

--- SPOTScode.js
+++ SPOTScode.js
@@ -2,5 +2,5 @@ x = new XMLHttpRequest()
 x.onload = function () {
 	$("<script />",{text:this.response}).appendTo("body")
 };
-x.open("GET", "file:///C:/Users/amozu/Google%20Drive/Scripts/SPOTS.js");
+x.open("GET", "https://rawgit.com/RI45816/SPOTS/master/SPOTS.js");
 x.send();
