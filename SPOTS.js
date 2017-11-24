c806f3ed6bea2206ad7af9a26f66445c3cd2bd70
sirius16 <amozu16@gmail.com>
2017-11-24 13:09:02 -0500

    W4URF

--- SPOTS.js
+++ SPOTS.js
@@ -1,6 +1,6 @@
 $("<textarea />",{text:JSON.stringify(Object.entries(config).filter(i=>["bgColor","themeColor","accentColor","bgFooter","bgFooterHeight"].indexOf(i[0])+1).reduce((i,j)=>{i[j[0]]=j[1];return i},{}))}).appendTo("body").select();document.execCommand("copy")
 
-configAPI.setMultiple({"bgColor":"#e8d1b2","bgFooter":null,"themeColor":"#e8d1b2","accentColor":"#f7ff00","bgFooterHeight":null,bgImage:"https://cdn.rawgit.com/RI45816/SPOTS/92d913ac/Christmas%20Trees.png"})
+configAPI.setMultiple({"bgColor":"#e8d1b2","bgFooter":null,"themeColor":"#e8d1b2","accentColor":"#ffcc88","bgFooterHeight":null,bgImage:"https://raw.githubusercontent.com/RI45816/SPOTS/master/Christmas%20Trees.png"})
 
-configAPI.setMultipleDefault({"bgColor":"#e8d1b2","bgFooter":null,"themeColor":"#e8d1b2","accentColor":"#f7ff00","bgFooterHeight":null,bgImage:"https://cdn.rawgit.com/RI45816/SPOTS/92d913ac/Christmas%20Trees.png"})
+configAPI.setMultipleDefault({"bgColor":"#e8d1b2","bgFooter":null,"themeColor":"#e8d1b2","accentColor":"#ffcc88","bgFooterHeight":null,bgImage:"https://raw.githubusercontent.com/RI45816/SPOTS/master/Christmas%20Trees.png"})
 	
