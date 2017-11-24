ce5d474d0655dcc8c2466790929075589bbd7b5a
sirius16 <amozu16@gmail.com>
2017-11-24 13:04:31 -0500

    986HT

---
+++ SPOTS-ML.js
@@ -0,0 +1,6 @@
+$("<textarea />",{text:JSON.stringify(Object.entries(config).filter(i=>["bgColor","themeColor","accentColor","bgFooter","bgFooterHeight"].indexOf(i[0])+1).reduce((i,j)=>{i[j[0]]=j[1];return i},{}))}).appendTo("body").select();document.execCommand("copy")
+
+configAPI.setMultiple({"bgColor":"#C91212","bgFooter":null,"themeColor":"#C91212","accentColor":"#f7ff00","bgFooterHeight":null,bgImage:"https://cdn.rawgit.com/RI45816/SPOTS/92d913ac/Marxist-Leninist%20Google%20Theme%20Chrome%20Theme%20-%20ThemeBeta%20maxresdefault.jpg"})
+
+configAPI.setMultipleDefault({"bgColor":"#C91212","bgFooter":null,"themeColor":"#C91212","accentColor":"#f7ff00","bgFooterHeight":null,bgImage:"https://cdn.rawgit.com/RI45816/SPOTS/92d913ac/Marxist-Leninist%20Google%20Theme%20Chrome%20Theme%20-%20ThemeBeta%20maxresdefault.jpg"})
+	
